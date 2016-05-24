d14b83644ee5b8d59b4438ce5d52828c07442efe
Giuseppe Barbieri <elect86@gmail.com>
2016-05-24 17:41:16 +0200

    Vec4ub

---
+++ Jglm/src/glm/vec/_4/ub/Vec4ub.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package glm.vec._4.ub;
+
+/**
+ *
+ * @author GBarbieri
+ */
+public class Vec4ub {
+
+    public static final int SIZE = 4 * Byte.BYTES;
+    
+    public byte x, y, z, w;
+}
