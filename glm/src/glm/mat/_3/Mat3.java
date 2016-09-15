0dc973d0ef702b3da4be0dce04230ce8dd63b273
fschaefers <florian.schaefers@gmail.com>
2016-09-15 14:39:03 +0200

    - Mat3 mul_(Vec3) added

--- glm/src/glm/mat/_3/Mat3.java
+++ glm/src/glm/mat/_3/Mat3.java
@@ -196,9 +196,13 @@ public class Mat3 extends matrixQuery {
     }
 
     public Vec3 mul(Vec3 v) {
+        return mul(v, new Vec3());
+    }
+    public Vec3 mul_(Vec3 v) {
         return mul(v, v);
     }
-
+    
+    
     public Vec3 mul(Vec3 right, Vec3 res) {
         res.set(m00 * right.x + m10 * right.y + m20 * right.z,
                 m01 * right.x + m11 * right.y + m21 * right.z,
